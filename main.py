64648b7cb696521de5cc3a37fbcd279e56438982
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-11-18 01:04:18 +0330

    simple exception handling implemented.

--- main.py
+++ main.py
@@ -39,7 +39,11 @@ def run_command(command):
     if command[-1] == '&':
         del command[-1]
     if command[0] == 'cd':
-        os.chdir(command[1])
+        try:
+            os.chdir(command[1])
+        except FileNotFoundError as e:
+            raise e
+
     else:
         child = os.fork()
         if child == 0:
@@ -66,24 +70,25 @@ def run_pipes(command):
     command = [com.strip().split() for com in command]
     read0, write0 = os.pipe()
     child1 = os.fork()
-    if child1 == 0:
-        #os.close(read0)
+    if child1 < 0:
+        raise Exception("Couldn't fork a child process!")
+    elif child1 == 0:
         with open('temp', 'w') as rd_output:
             os.dup2(rd_output.fileno(), sys.stdout.fileno())
-        #os.dup2(sys.stdout.fileno(),write0)
         os.execvp(command[0][0], args=command[0])
         sys.exit(0)
     os.wait()
     child2 = os.fork()
-    if child2 == 0:
-            #os.dup2(sys.stdin.fileno(),read0)
-            #os.close(write0)
+    if child2 < 0:
+        raise Exception("Couldn't fork a child process!")
+    elif child2 == 0:
         with open('temp', 'r') as rd_input:
             os.dup2(rd_input.fileno(), sys.stdin.fileno())
         os.execvp(command[1][0], command[1])
         sys.exit()
     os.wait()
     os.remove('temp')
+
 
 if __name__ == '__main__':
     shell()
